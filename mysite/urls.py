0e8182afe3b1a9640b9bc3cefb3bd9e967813d69
Lavanya <lavanyamg@gmail.com>
2015-07-11 16:22:47 +1000

    Changed the HTML for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
+    url(r'', include('blog.urls')),
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
